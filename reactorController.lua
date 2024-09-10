e5bc1ccfd94cab008a5fcbb51f5f62197ef788f8
Arnaud Léonard <arnaud.leonard@infomaniak.com>
2024-09-10 11:28:57 +0200

    feat: initialisation for reactorController.lua

---
+++ reactorController.lua
@@ -0,0 +1,8 @@
+local component = require("component")
+local sides = require("sides")
+local event = require("event")
+
+-- configure local variables
+local outputLabel = "Silver Plasma"
+local input1Label = "Gold Dust"
+local input2Label = "Arsenic Dust"
